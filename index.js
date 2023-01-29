9ac254574abcc26a7c17a1612a45504b8bc03c4b
gagan-28deep <gagan_28_deep@outlook.com>
2023-01-30 00:08:29 +0530

    updated Access origin

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ mongoose
 // Middleware
 app.use(express.json());
 app.use(fileUpload());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // app.use(
 //   cors({
 //     allowedHeaders: ["sessionId", "Content-Type"],
